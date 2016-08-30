48706b37258907aaeb2aa312614a1118019b9625
olaola <yapetrenka@yandex.ru>
2016-08-30 16:28:45 +0300

    slick-slider

--- html/index.php
+++ html/index.php
@@ -5,7 +5,15 @@ include("../templates/main/header.tpl.php");
 ?>
 
 <div class="layout-main">
-index
+    <div class="main-slider__wrap">
+        <div class="main-slider">
+            <?for ($i=1; $i<=8; $i++) {?>
+            <div class="main-slider__item">
+                <a href="/uploads/images/slider/big/slide_img_<?=$i?>.jpg" data-exthumbimage="/uploads/images/slider/thumb/slide_img_<?=$i?>.jpg"><img src="/uploads/images/slider/prev/slide_img_<?=$i?>.jpg" alt=""></a>
+            </div>
+            <?}?>
+        </div>
+    </div>
 </div>
 
 <?
